4b5c4c5ed7998b05240c8a110ff3c83d6a0c6ac7
blevinspercussion <blevinspercussion@gmail.com>
2022-07-17 09:55:37 -0500

    added Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import './components.css';
+
+function Header () {
+    return (
+        <div>
+            <h1>Adam Blevins</h1>
+            <h2>Drummer Who Codes</h2>
+        </div>
+    )
+}
+
+export default Header;
