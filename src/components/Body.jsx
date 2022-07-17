14b83219fec7bbc9958049c454431774e85b07fb
blevinspercussion <blevinspercussion@gmail.com>
2022-07-17 10:15:42 -0500

    Added content and minor styling to Body component

---
+++ src/components/Body.jsx
@@ -0,0 +1,30 @@
+import './components.css';
+
+function Body () {
+    return (
+        <div className='text-block'>
+            <div></div>
+            <p>
+                My name is Adam, a.k.a. 'Drummer Who Codes', and I am an aspiring front end web 
+                developer. My preferred tech stack is MERN, but I have also dabbled in Django for 
+                backend work. I am comfortable working in HTML5, CSS3, and vanilla Javascript (ES6) 
+                as well as React. I am also open to learning new technologies as the job may require.
+            </p>
+            <p>
+                I believe in simple, effective design and simple, effective solutions.
+            </p>
+
+            <p>
+                For the last eleven years I have worked as a professional musician, but I am looking 
+                to make a career change into the technology industry. My background as a lifelong 
+                musician has taught me invaluable soft skills, such as: Perseverence, persitence, 
+                communication, self-teaching, being coachable, and active listening. I am looking 
+                forward to taking these skills, as well as my newfound tech skills, into a exciting 
+                new opportunities.
+            </p>
+            <div></div>
+        </div>
+    )
+}
+
+export default Body;
